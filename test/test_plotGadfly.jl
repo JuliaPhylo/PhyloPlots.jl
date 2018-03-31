5aa9bcb766041e9f99a38095cb2bc028af5e1232
Cecile Ane <cecileane@users.noreply.github.com>
2018-03-31 15:01:03 -0500

    extra test for Gadfly

--- test/test_plotGadfly.jl
+++ test/test_plotGadfly.jl
@@ -14,4 +14,13 @@
   @test_nowarn plot(net, edgeColor=colorant"olive", # uses Colors
           minorHybridEdgeColor=colorant"tan",
           majorHybridEdgeColor=colorant"skyblue");
+
+  # example with weird plot
+  cui3str = "(Xmayae,((Xhellerii,(((Xclemenciae_F2,Xmonticolus):1.458,(((((Xmontezumae,(Xnezahuacoyotl)#H26:0.247::0.804):0.375,((Xbirchmanni_GARC,Xmalinche_CHIC2):0.997,Xcortezi):0.455):0.63,(#H26:0.0::0.196,((Xcontinens,Xpygmaeus):1.932,(Xnigrensis,Xmultilineatus):1.401):0.042):2.439):2.0)#H7:0.787::0.835,(Xmaculatus,(Xandersi,(Xmilleri,((Xxiphidium,#H7:9.563::0.165):1.409,(Xevelynae,(Xvariatus,(Xcouchianus,(Xgordoni,Xmeyeri):0.263):3.532):0.642):0.411):0.295):0.468):0.654):1.022):0.788):1.917)#H27:0.149::0.572):0.668,Xalvarezi):0.257,(Xsignum,#H27:1.381::0.428):4.669);"
+  net3  = readTopology(cui3str);
+  deleteleaf!(net3,"Xhellerii"); deleteleaf!(net3,"Xsignum");
+  deleteleaf!(net3,"Xmayae", simplify=false);
+  @test_nowarn plot(net3); # looks weird: k=2 cycle at the root. 3 root edges:
+  # one to a leaf, 1 major & 1 minor hybrid edge to the same child.
+
 end

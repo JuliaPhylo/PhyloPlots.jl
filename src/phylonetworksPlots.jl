e9604a51ec0db46f726472142de1fffce704711e
Cecile Ane <cecileane@users.noreply.github.com>
2021-10-17 16:38:40 -0500

    fix in gadfly to show minor edges

--- src/phylonetworksPlots.jl
+++ src/phylonetworksPlots.jl
@@ -2,12 +2,46 @@
 # calculate plotting coordinates of nodes & edges for PhyloNetworks objects
 
 """
-    getEdgeNodeCoordinates(net, useEdgeLength)
+    getEdgeNodeCoordinates(net, useEdgeLength::Bool, useSimpleHybridLines::Bool)
 
 Calculate coordinates for plotting later with Gadfly or RCall.
 
 Actually modifies some (minor) attributes of the network,
 as it calls `directEdges!`, `preorder!` and `cladewiseorder!`.
+
+output: tuple with the following elements, in which the order of
+nodes corresponds to the order in `net.node`, and the order of
+edges corresponds to that in `net.edge` (filtered to minor edges as needed).
+
+1. `edge_xB`: x coordinate for the Beginning and ...
+2. `edge_xE`: ...  End of each edge, in the same order as in `net.edge`
+3. `edge_yB`: y coordinate for edges, Begin ...
+4. `edge_yE`: ... and End.
+   Each major edge is drawn as a single horizontal line. Minor hybrid edges are
+   drawn as: a single diagonal segment if `useSimpleHybridLines` is true,
+   or as 2 connected segments otherwise: one horizontal (whose length on the
+   x axis can be used to represent the edge length), and the other diagonal to
+   connect the horizontal segment to the child node.
+   edge_* contains the coordinates for the horizontal segment only, which is
+   reduced to a single point (Begin = End) when using "SimpleHybridLines".
+   minoredge_* (see below) contains information for the diagonal segment.
+   Agreed, edge_yB = edge_yE always (relic from before v0.3:
+   no minoredge output back then, and simple diagonal lines only)
+5. `node_x`: x and ...
+6. `node_y`: ... y coordinate at the middle of the vertical bar that represents a node.
+   The (or each) parent edge of the node connects to this middle point,
+   but the node itself is drawn as a vertical bar connected to all it children edges.
+   order: same as in `net.node`
+7. `node_yB`: y coordinates of the Beginning and the ...
+8. `node_yE`: ... End of the vertical bar representing each node.
+   The x coordinate (Begin & End) of the end points of the vertical bar is
+   the same as that of the mid-point, given by `node_x`.
+9. `minoredge_xB`: x coordinate for the Beginning and ...
+10. `minoredge_xE`: ... End of the diagonal segment of each minor hybrid edge,
+   in the same order as in `filter(e -> !e.isMajor, net.edge)`.
+11. `minoredge_yB`: y coordinate for the beginning and ...
+12. `minoredge_yE`: ... end of the diagonal segment of each minor hybrid edge.
+13-16. `xmin`, `xmax`, `ymin`, `ymax`: ranges for the x and y axes.
 """
 function getEdgeNodeCoordinates(net::HybridNetwork, useEdgeLength::Bool, useSimpleHybridLines::Bool)
     try

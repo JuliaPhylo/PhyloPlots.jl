43609881962972eae6bb9547f8ab5dcea80d239e
Cecile Ane <cecileane@users.noreply.github.com>
2026-07-04 14:29:51 +0200

    tests adjustments: cont

--- test/test_plotRCall.jl
+++ test/test_plotRCall.jl
@@ -8,17 +8,18 @@
   net = readnewick("(((Ag,(#H1:7.159::0.056,((Ak,(E:0.08,#H2:0.0::0.004):0.023):0.078,(M:0.0)#H2:::0.996):2.49):2.214):0.026,(((((Az:0.002,Ag2:0.023):2.11,As:2.027):1.697)#H1:0.0::0.944,Ap):0.187,Ar):0.723):5.943,(P,20):1.863,165);");
 
   @test_logs plot(net);
-  @test_logs plot(net, edgewidth=Dict(1=>4, 28=>28))
-  @test_logs (:warn, "At least one non-missing edge length: plotting any missing length as 1.0") plot(net, useedgelength=true);
-  @test_logs plot(net, showtiplabel=false);
-  @test_logs plot(net, shownodenumber=true, shownodelabel=true);
-  @test_logs plot(net, tipoffset=1, showgamma=true, minorlinetype=3); # 3=dotted
+  @test_logs plot(net, curved=:both, edgewidth=Dict(1=>4, 9=>4, 2=>4, 28=>28))
+  @test_logs (:warn, "At least one non-missing edge length: plotting any missing length as 1.0") plot(net,
+    useedgelength=true, curved=:minor);
+  @test_logs plot(net, showtiplabel=false, style=:majortree, curved=:minor);
+  @test_logs plot(net, shownodenumber=true, shownodelabel=true, style=:majortree, curved=:both);
+  @test_logs plot(net, tipoffset=1, showgamma=true, minorlinetype=3, style=:majortree); # 3=dotted
   @test_logs plot(net, showedgelength=true, showedgenumber=true);
   @test_logs plot(net, edgecolor="tomato4", minorhybridedgecolor="skyblue",
-          majorhybridedgecolor="tan");
+          majorhybridedgecolor="tan", curved=:minor);
   dat = DataFrame(node=[-5,-10,-1],bs=["90","95","100"],edge=[11,22,26]);
   @test_logs (:warn, "Some node numbers in the nodelabel data frame are not found in the network:\n -1") plot(net, nodelabel=dat);
-  @test_logs plot(net, edgelabel=dat[!,[:edge,:bs]]);
+  @test_logs plot(net, edgelabel=dat[!,[:edge,:bs]], style=:majortree, curved=:both);
 
   @test_logs plot(net, style=:majortree, arrowlen=0.1);
   @test_logs (:warn, "Style bogus is unknown. Defaulted to :fulltree.") plot(net, style=:bogus);
